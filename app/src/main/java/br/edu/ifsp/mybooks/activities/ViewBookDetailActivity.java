e128c1e9546023b93764c7f4f72679895345f1d3
camila-borges <camila_nami@hotmail.com>
2017-11-11 22:04:23 -0200

    First commit

---
+++ app/src/main/java/br/edu/ifsp/mybooks/activities/ViewBookDetailActivity.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.mybooks.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.ifsp.mybooks.R;
+
+public class ViewBookDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_book_detail);
+    }
+}

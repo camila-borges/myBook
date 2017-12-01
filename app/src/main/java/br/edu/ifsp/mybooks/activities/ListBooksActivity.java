1ef9bfbce8fc0daf53c47b25480728c2b4fe7f83
camila-borges <camila_nami@hotmail.com>
2017-12-01 17:53:42 -0200

    App DONE]

--- app/src/main/java/br/edu/ifsp/mybooks/activities/ListBooksActivity.java
+++ app/src/main/java/br/edu/ifsp/mybooks/activities/ListBooksActivity.java
@@ -2,11 +2,15 @@ package br.edu.ifsp.mybooks.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,26 +30,57 @@ public class ListBooksActivity extends AppCompatActivity {
     private Adapter adapter;
     private EditText textBook;
 
+
+
     private List<Livro> arrayList;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater minfl = getMenuInflater();
+        minfl.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuItem1:
+                String textMessageBox = "Essa aplicação foi desenvolvida por Camila, tem como objetivo a criação de uma " +
+                        "biblioteca virtual própria. Para trabalhos futuros o objetivo será transformar esta aplicação" +
+                        " em uma rede social para compartilhamento de informações, sobre livros entre os usuários.";
+                messageBox("Sobre", textMessageBox);
+                break;
+            case R.id.menuItem2:
+                Intent intent = new Intent(ListBooksActivity.this, ReadIsbnCodeActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_books);
-        addbtn = (Button) findViewById(R.id.bttonadd);
+//        addbtn = (Button) findViewById(R.id.bttonadd);
         textBook = (EditText) findViewById(R.id.searchBookList);
         DatabaseHandler db = new DatabaseHandler(this);
         arrayList = db.getAllLivros();
         prepareRecyclerView();
 
 
-        addbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ListBooksActivity.this, ReadIsbnCodeActivity.class);
-                startActivity(intent);
-            }
-        });
+//        addbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(ListBooksActivity.this, ReadIsbnCodeActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
         textBook.addTextChangedListener(new TextWatcher() {
             @Override
@@ -78,6 +113,17 @@ public class ListBooksActivity extends AppCompatActivity {
         super.onResume();
         DatabaseHandler db = new DatabaseHandler(this);
         arrayList = db.getAllLivros();
+    }
+
+
+    private void messageBox(String title, String message) {
+
+        AlertDialog.Builder messageBox = new AlertDialog.Builder(this);
+        messageBox.setTitle(title);
+        messageBox.setMessage(message);
+        messageBox.setCancelable(false);
+        messageBox.setNeutralButton("OK", null);
+        messageBox.show();
     }
 
     private void prepareRecyclerView() {
